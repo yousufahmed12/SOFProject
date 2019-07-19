a15dc430fb8c0f7f14aa52825a2c983c0e5a56c6
naibingjiang <naibingjiang@gmail.com>
2019-07-19 11:14:31 -0400

    organized

--- Javascript/SOF-Angular/src/app/models/Item.ts
+++
@@ -1,6 +0,0 @@
-export class Item {
-    stackId: string;
-    badge: number[];
-    answersLinks: string[];
-    commentsLinks: string[];
-}
--- Javascript/SOF-Angular/src/app/models/StackCandidate.ts
+++
@@ -1,6 +0,0 @@
-export class StackCandidate {
-    stackId: string;
-    badge: number[];
-    answersLinks: string[];
-    commentsLinks: string[];
-}
